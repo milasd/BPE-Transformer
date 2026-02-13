7a134b3e75f0afdf243b621b03110b196f7a6862
milasd <milasoaresdeoliveira@gmail.com>
2026-02-13 20:07:04 -0300

    Update gradient clipping w/ batch operations

--- bpe_transformer/optimizer/utils/gradient_clipping.py
+++ bpe_transformer/optimizer/utils/gradient_clipping.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import torch
 from collections.abc import Iterable
 
@@ -9,12 +10,20 @@ def gradient_clipping(params: Iterable[torch.nn.Parameter], max_l2_norm: float) 
         params: Iterable of parameters with gradients
         max_l2_norm: Maximum L2 norm
     """
-    # Compute total L2 norm across all parameter gradients
-    total_norm = torch.sqrt(sum(p.grad.norm() ** 2 for p in params if p.grad is not None))
+    # Collect gradients by device
+    grads_by_device = defaultdict(list)
+
+    for p in params:
+        if p.grad is not None:
+            grads_by_device[p.grad.device].append(p.grad)
+
+    # Compute total L2 norm across all parameter gradients, across all devices
+    total_norm = torch.sqrt(sum(sum(g.norm() ** 2 for g in grads) for grads in grads_by_device.values()))
 
     # Clip if necessary
     if total_norm > max_l2_norm:
         clip_coef = max_l2_norm / (total_norm + 1e-6)
-        for p in params:
-            if p.grad is not None:
-                p.grad.mul_(clip_coef)
+
+        # Apply clipping using foreach for each device.
+        for grads in grads_by_device.values():
+            torch._foreach_mul_(grads, clip_coef)
